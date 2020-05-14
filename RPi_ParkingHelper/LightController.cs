77543d6efc73605420c0b9ce76b73087416fc924
marcin <marcinkozikowski@wp.pl>
2020-05-15 00:00:13 +0200

    init parking system c#

---
+++ RPi_ParkingHelper/LightController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Gpio;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPi_ParkingHelper
+{
+    internal class LightController
+    {
+        private readonly int _green;
+        private readonly int _yellow;
+        private readonly int _red;
+        private readonly GpioController _gpio;
+
+        public int GreenLight => _green;
+        public int YellowLight => _yellow;
+        public int RedLight => _red;
+
+        public LightController(GpioController gpio)
+        {
+            _green = 22;
+            _yellow = 27;
+            _red = 17;
+            _gpio = gpio;
+        }
+        public LightController(GpioController gpio,int redLightPin,int yellowLightPin, int greenLightPin)
+        {
+            _green = greenLightPin;
+            _yellow = yellowLightPin;
+            _red = redLightPin;
+            _gpio = gpio;
+        }
+
+        public void TurnOnLight(Lights light)
+        {
+            _gpio.Write((int)light, PinValue.Low);
+        }
+        public void TurnOffLight(Lights light)
+        {
+            _gpio.Write((int)light, PinValue.High);
+        }
+
+        public Lights InterprateDistance(double distance)
+        {
+            if (distance > 0 && distance < 20)
+            {
+                TurnOffLight(Lights.Green);
+                TurnOffLight(Lights.Yellow);
+                TurnOnLight(Lights.Red);
+                return Lights.Red;
+            }
+            else if (distance > 20 && distance < 60)
+            {
+                TurnOffLight(Lights.Green);
+                TurnOnLight(Lights.Yellow);
+                TurnOffLight(Lights.Red);
+                return Lights.Yellow;
+            }
+            else if (distance > 60)
+            {
+                TurnOnLight(Lights.Green);
+                TurnOffLight(Lights.Yellow);
+                TurnOffLight(Lights.Red);
+                return Lights.Green;
+            }
+            return Lights.Red;
+        }
+
+        public void TutnOffAllLights()
+        {
+            TurnOffLight(Lights.Green);
+            TurnOffLight(Lights.Yellow);
+            TurnOffLight(Lights.Red);
+        }
+    }
+
+    public enum Lights
+    {
+        Green=22,
+        Yellow=27,
+        Red=17,
+    }
+}
---
+++ RPi_ParkingHelper/MainPage.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Gpio;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace RPi_ParkingHelper
+{
+    /// <summary>
+    /// Pusta strona, która może być używana samodzielnie lub do której można nawigować wewnątrz ramki.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private LightController _lightController;
+        private DispatcherTimer _timer;
+        private int[] _pinNumbers = new[] { 17, 27, 22 };
+        private HCSR04 ultrasonicMeter;
+        private GpioController _gpio;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            if (InitGPIO())
+            {
+                InitTimer();
+            }
+        }
+
+        private void InitTimer()
+        {
+            _lightController.TurnOffLight(RPi_ParkingHelper.Lights.Green);
+            _lightController.TurnOffLight(RPi_ParkingHelper.Lights.Yellow);
+            _lightController.TurnOffLight(RPi_ParkingHelper.Lights.Red);
+
+            var distances = new Queue<Lights>();
+            //var lights = new[] { RedLed, YellowLed, GreenLed };
+            _timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(1200) };
+            _timer.Tick += (s, e) =>
+            {
+                _timer.Stop();
+                double distance = 0;
+                var isValid = ultrasonicMeter.GetDistance(out distance);
+                if(isValid)
+                {
+                    var li = _lightController.InterprateDistance(distance);
+                    distances.Enqueue(li);
+                }
+                if(!IsStillMoving(distances))
+                {
+                    _lightController.TutnOffAllLights();
+                }
+                _timer.Start();
+            };
+            _timer.Start();
+        }
+
+        private bool IsStillMoving(Queue<Lights> distances)
+        {
+            if(distances.Count() == 10)
+            {
+                foreach (var li in distances)
+                {
+                    if (li != distances.ElementAt(0))
+                    {
+                        distances.Dequeue();
+                        return true;
+                    }
+                }
+                distances.Dequeue();
+                return false;
+            }
+            return true;
+        }
+
+        private bool InitGPIO()
+        {
+            _gpio = new GpioController(PinNumberingScheme.Logical);
+            _lightController = new LightController(_gpio);
+            if (_gpio == null)
+                return false;
+            for (int i = 0; i < 3; i++)
+            {
+                _gpio.OpenPin(_pinNumbers[i],PinMode.Output);
+                _gpio.Write(_pinNumbers[i], PinValue.High);
+            }
+
+            ultrasonicMeter = new HCSR04(_gpio, 23, 24, true);
+
+            return true;
+        }
+    }
+
+    
+}
